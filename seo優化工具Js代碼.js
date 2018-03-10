56a30a13b58a14e9f8dc8598b9f27fa4b08ec524
洪嘉駿 <yf9000555@gmail.com>
2018-03-11 01:22:18 +0800

    關鍵字seo優化工具的Js代碼
    有黑名單與優化關鍵字的設定

---
+++ seo優化工具Js代碼.js
@@ -0,0 +1,68 @@
+function ShowSearchFirst(BlockString,SeoString="")
+{
+	var canclick=true;
+	var SearchText = [document.getElementsByTagName('h3')[0],document.getElementsByTagName('h3')[1],document.getElementsByTagName('h3')[2]];
+	
+	var Blockli=BlockString.split(",")
+	var Seoli=SeoString.split(",")
+	if (BlockString=="")
+	{
+		return "No BlockString list input";
+	}
+	Blockli.forEach(function(BlockKeyword)
+	{
+		if (BlockKeyword!="")
+		{
+			var num=0;
+		SearchText.forEach(function(SearchTextele)
+		{
+			if (SearchTextele.innerHTML.indexOf(BlockKeyword)> -1)
+			{
+				SearchText.splice(num,1);
+			}
+			num+=1;
+		});
+		}
+	});
+	if (SeoString=="")
+	{
+ 		console.log(SearchText);
+ 		SearchText.forEach(function(SearchTextele)
+		{
+			if (SearchTextele!=null)
+			{
+				if (canclick)
+				{
+					canclick=false;
+					SearchTextele.click();	
+				}
+			}
+			
+		});
+	}
+	else if (SeoString!="")
+	{
+		SearchText.forEach(function(SearchTextele)
+		{
+			Seoli.forEach(function(SeoKeyword)
+			{
+				if (SeoKeyword!="")
+				{
+					console.log("SeoKeyword:"+SeoKeyword)
+					console.log(SearchTextele.innerHTML.indexOf(SeoKeyword).toString())
+					console.log(SearchTextele.innerHTML)
+			  		if (SearchTextele.innerHTML.indexOf(SeoKeyword)> -1)
+			  		{
+			  			if (canclick)
+			  			{
+			  				canclick=false;
+			  				console.log("Click: "+SearchTextele.innerHTML)
+			  				SearchTextele.click();
+			  			}
+			  		}
+			  		console.log("-----------------")
+				}
+			});
+		});
+	}
+}
