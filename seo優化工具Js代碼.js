e4b9f42172e1ee1eaf09cea32153b57ec0f797a6
洪嘉駿 <yf9000555@gmail.com>
2018-04-26 14:39:36 +0800

    Update seo優化工具Js代碼.js
    新增版型修改的判斷

--- seo優化工具Js代碼.js
+++ seo優化工具Js代碼.js
@@ -1,93 +1,62 @@
-function ShowSearchFirst(BlockString,SeoString='')
-{
-	var canclick=true;
-	//var SearchText = [document.getElementsByClassName('ad_cclk')[0].getElementsByTagName('a')[1],document.getElementsByClassName('ad_cclk')[1].getElementsByTagName('a')[1],document.getElementsByClassName('ad_cclk')[2].getElementsByTagName('a')[1]];
-	var SearchText=[,,];
-	try
-	{
-		SearchText[0]= document.getElementsByClassName('ad_cclk')[0].getElementsByTagName('a')[1].getElementsByTagName('div')[0];
-	}
-	catch(e)
-	{
-
-	}
-	try
-	{
-		SearchText[1]= document.getElementsByClassName('ad_cclk')[1].getElementsByTagName('a')[1].getElementsByTagName('div')[0];
-	}
-	catch(e)
-	{
-		
-	}
-	try
-	{
-		SearchText[2]= document.getElementsByClassName('ad_cclk')[2].getElementsByTagName('a')[1].getElementsByTagName('div')[0];
-	}
-	catch(e)
-	{
-		
-	}
-	var Blockli=BlockString.split(',');
-	var Seoli=SeoString.split(',');
-	Blockli.forEach(function(BlockKeyword)
-	{
-		if (BlockKeyword!='')
-		{
-			var num=0;
-		SearchText.forEach(function(SearchTextele)
-		{
-			if (SearchTextele.innerHTML.indexOf(BlockKeyword)> -1)
-			{
-				SearchText.splice(num,1);
-			}
-			num+=1;
-		});
+function ShowSearchFirst(BlockString, SeoString = '') {
+	var canclick = true;
+	var SearchText = [, , ];
+	try {
+		SearchText[0] = document.getElementsByClassName('ads-fr')[0]
+	} catch(e) {}
+	try {
+		SearchText[1] = document.getElementsByClassName('ads-fr')[1]
+	} catch(e) {}
+	try {
+		SearchText[2] = document.getElementsByClassName('ads-fr')[2]
+	} catch(e) {}
+	var Blockli = BlockString.split(',');
+	var Seoli = SeoString.split(',');
+	Blockli.forEach(function(BlockKeyword) {
+		if (BlockKeyword != '') {
+			var num = 0;
+			SearchText.forEach(function(SearchTextele) {
+				if (SearchTextele.innerHTML.indexOf(BlockKeyword) > -1) {
+					SearchText.splice(num, 1)
+				}
+				num += 1
+			})
 		}
 	});
-	if (SeoString=='')
-	{
-		alert(SearchTextele.innerHTML);
- 		//console.log(SearchText);
- 		SearchText.forEach(function(SearchTextele)
-		{
-			if (SearchTextele!=null)
-			{
-				if (canclick)
-				{
-					canclick=false;
-					SearchTextele.click();	
+	if (SeoString == '') {
+		SearchText.forEach(function(SearchTextele) {
+			if (SearchTextele != null) {
+				if (canclick) {
+					canclick = false;
+					SearchTextele.getElementsByTagName('div')[4].getElementsByTagName('span')[1].click()
 				}
 			}
-			
-		});
-	}
-	else if (SeoString!='')
-	{
-		SearchText.forEach(function(SearchTextele)
-		{
-			Seoli.forEach(function(SeoKeyword)
-			{
-				if (SeoKeyword!='')
-				{
-					console.log(SearchText[0])
-					console.log(SearchText[1])
-					console.log(SearchText[2])
-					//console.log(SearchTextele.innerHTML);
-					//alert(SearchTextele.innerHTML);
-			  		if (SearchTextele.innerHTML.indexOf(SeoKeyword)> -1)
-			  		{
-			  			if (canclick)
-			  			{
-			  				canclick=false;
-			  				console.log('Click: '+SearchTextele.innerHTML);
-			  				SearchTextele.click();
-			  			}
-			  		}
-			  		console.log('-----------------');
+		})
+	} else if (SeoString != '') {
+		SearchText.forEach(function(SearchTextele) {
+			Seoli.forEach(function(SeoKeyword) {
+				if (SeoKeyword != '') {
+					console.log(SearchText[0].getElementsByTagName('div')[2].getElementsByTagName('div')[0].innerHTML);
+					console.log(SearchText[1].getElementsByTagName('div')[2].getElementsByTagName('div')[0].innerHTML);
+					console.log(SearchText[2].getElementsByTagName('div')[2].getElementsByTagName('div')[0].innerHTML);
+					if (SearchTextele.getElementsByTagName('div')[2].getElementsByTagName('a')[0].getElementsByTagName('div')[0].innerHTML.indexOf(SeoKeyword) > -1) {
+						if (canclick) {
+							canclick = false;
+							console.log('Click: ' + SearchTextele.getElementsByTagName('div')[2].getElementsByTagName('a')[0].getElementsByTagName('div')[0].innerHTML);
+							SearchTextele.getElementsByTagName('div')[4].getElementsByTagName('span')[1].click()
+						}
+					}
+					if (SearchTextele.getElementsByTagName('div')[2].getElementsByTagName('a')[0].getElementsByTagName('div')[1].innerHTML.indexOf(SeoKeyword) > -1) {
+						if (canclick) {
+							canclick = false;
+							console.log('Click: ' + SearchTextele.getElementsByTagName('div')[2].getElementsByTagName('a')[0].getElementsByTagName('div')[1].innerHTML);
+							SearchTextele.getElementsByTagName('div')[3].click()
+						}
+					}
+					console.log('-----------------')
 				}
-			});
+			})
 		});
-		return SearchText;
+		return SearchText
 	}
 }
-//Js 去格式化網站   http://www.sioe.cn/xinqing/format-js.php
