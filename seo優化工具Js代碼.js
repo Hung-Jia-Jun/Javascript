deb7927418f3109eed57bc36647d6c3d7aff1df3
洪嘉駿 <yf9000555@gmail.com>
2018-04-28 15:31:17 +0800

    Update seo優化工具Js代碼.js

--- seo優化工具Js代碼.js
+++ seo優化工具Js代碼.js
@@ -1,19 +1,23 @@
-function eraseCookie(name) {
-	document.cookie = name + '=; Max-Age=-99999999;';
+function eraseCookie(name)
+{
+	console.log(name+'=; Max-Age=-99999999;');
+	document.cookie = name+'=; Max-Age=-99999999;';
 }
+
 function ShowSearchFirst(BlockString, SeoString) {
 	var cookies = document.cookie.split(";");
-	for (var i = 0; i < cookies.length; i++) eraseCookie(cookies[i].split("=")[0]);
+	for (var i = 0; i < cookies.length; i++)
+		eraseCookie(cookies[i].split("=")[0]);
 	var canclick = true;
 	var SearchText = [, , ];
 	try {
-		SearchText[0] = document.getElementsByClassName('ads-fr')[0].getElementsByTagName('div')[2];
+		SearchText[0] = document.getElementsByClassName('ads-fr')[0];
 	} catch(e) {}
 	try {
-		SearchText[1] = document.getElementsByClassName('ads-fr')[1].getElementsByTagName('div')[2];
+		SearchText[1] = document.getElementsByClassName('ads-fr')[1];
 	} catch(e) {}
 	try {
-		SearchText[2] = document.getElementsByClassName('ads-fr')[2].getElementsByTagName('div')[2];
+		SearchText[2] = document.getElementsByClassName('ads-fr')[2];
 	} catch(e) {}
 	var Blockli = BlockString.split(',');
 	var Seoli = SeoString.split(',');
@@ -42,24 +46,30 @@ function ShowSearchFirst(BlockString, SeoString) {
 			Seoli.forEach(function(SeoKeyword) {
 				if (SeoKeyword != '') {
 					try {
-						console.log(SearchText[0].getElementsByTagName('div')[0].innerHTML);
+						console.log(SearchText[0].getElementsByTagName('div')[2].getElementsByTagName('div')[0].innerHTML);
 					} catch(e) {}
 					try {
-						console.log(SearchText[1].getElementsByTagName('div')[0].innerHTML);
+						console.log(SearchText[1].getElementsByTagName('div')[2].getElementsByTagName('div')[0].innerHTML);
 					} catch(e) {}
 					try {
-						console.log(SearchText[2].getElementsByTagName('div')[0].innerHTML);
+						console.log(SearchText[2].getElementsByTagName('div')[2].getElementsByTagName('div')[0].innerHTML);
 					} catch(e) {}
 					if (SearchTextele.getElementsByTagName('div')[0].innerHTML.indexOf(SeoKeyword) > -1) {
 						if (canclick) {
 							canclick = false;
 							console.log('Click: ' + SearchTextele.getElementsByTagName('div')[0].innerHTML);
-							try {
-								SearchTextele.getElementsByTagName('div')[1].click();
-							} catch(e) {}
-							try {
-								SearchTextele.getElementsByTagName('div')[0].click();
-							} catch(e) {}
+							try
+							{
+								SearchTextele.getElementsByTagName('div')[2].getElementsByTagName('div')[1].click();
+							}
+							catch(e)
+							{}
+							try
+							{
+								SearchTextele.getElementsByTagName('div')[2].getElementsByTagName('div')[0].click();
+							}
+							catch(e)
+							{}
 						}
 					}
 					console.log('-----------------')
